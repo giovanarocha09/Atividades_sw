368fe08ab868bd7357775f44b99443c9d8b2af47
Giovana Rocha <100931504+giovanarochaz@users.noreply.github.com>
2023-04-09 13:36:48 -0300

    Exercício - Aluno

---
+++ Aula_14_03_23/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_Aluno;
+class Program
+{
+    static void Main(string[] args){
+
+        Aluno aluno1 = new Aluno();
+        
+        Console.WriteLine();
+        Console.Write("Digite seu nome: ");
+        aluno1.nome = Console.ReadLine();
+
+        Console.Write("Digite a primeira nota( no modo double): ");
+        aluno1.nota1 = double.Parse(Console.ReadLine());
+        
+        Console.Write("Digite a segunda nota( no modo double): ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
+        
+        aluno1.mensagem();
+        Console.WriteLine();
+    }
+}
