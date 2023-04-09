e267f2acd2d0db0a4ecde07bbe230f2ac4f799b9
Giovana Rocha <100931504+giovanarochaz@users.noreply.github.com>
2023-04-09 13:21:28 -0300

    Exercício - Conta Bancária

---
+++ Aula_28_03_23/conta_bancaria/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace conta_bancaria;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Conta bancaria");
+        Conta conta = new Conta();
+
+        conta.Depositar(500);
+        conta.AjustaLimite(500);
+        conta.Sacar(900);
+
+        Console.WriteLine("Valor total na sua conta: "+ conta.MostraSaldo());
+    }
+}
