7e04a60ae92eaa761dceb0c87b94b48fc9256285
Giovana Rocha <100931504+giovanarochaz@users.noreply.github.com>
2023-04-11 11:39:07 -0300

    Agregação

---
+++ Aula_11_04_23/agregacao/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace agregacao;
+class Program
+{
+    static void Main(string[] args){
+
+        Console.WriteLine("TRABALHANDO COM AGREGAÇÃO");
+
+        //Como instanciar um objeto
+
+        Cartao cdc = new Cartao();
+        Cliente cli = new Cliente();
+
+        //Nome para cliente
+        cli.Nome = "Gih Rocha";
+
+        //Numero e Validade do Cartao
+        cdc.Numero = "12344321";
+        cdc.Validade = "23/2025";
+
+        //Associando Cliente ao atributo de agregação do cartao
+        cdc.Cliente = cli;
+
+        //Visualização 
+
+        Console.WriteLine("O Nome do Cliente: " + cli.Nome);
+        Console.WriteLine("O número do cartão: " + cdc.Numero);
+        Console.WriteLine("A data de Validade: " + cdc.Validade);
+        Console.WriteLine("O Nome do Cliente AGREGADO: " + cdc.Cliente.Nome);
+
+    }
+}
