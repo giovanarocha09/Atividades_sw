6d5732319da309e9f844e22018922f145c4ba170
Giovana Rocha <100931504+giovanarochaz@users.noreply.github.com>
2023-04-18 12:34:57 -0300

    Avaliação - banco

---
+++ Aula_18_04_23/Conta.cs
@@ -0,0 +1,35 @@
+namespace conta_bancaria;
+class Conta{
+
+    public double Valor;
+    public string? cliente{get;set;}
+    public double Saldo{get;set;}
+    public double Limite{get;set;}
+
+    public double obterjuros(){
+        double Juros = ((Valor * 0.06)*12) + Valor;
+        return Juros;
+    }
+
+    public double Mensal(){
+        double Mes = (Valor * 0.06 + Valor/12);
+        return Mes;
+    }
+
+    public void Deposito(double custo){
+        this.Saldo += Valor;
+    }
+
+
+    public void Saque(double custo){
+        this.Saldo -= Valor;
+    }
+
+    public double ConsutSaldo(){
+        return this.Saldo += this.Limite;
+    }
+
+
+
+
+}
---
+++ Aula_18_04_23/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace conta_bancaria;
+class Program
+{
+    static void Main(string[] args){
+
+        Conta conta = new Conta();
+
+        Console.Write("Digite o valor do seu emprestimo: ");
+        conta.Valor = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Seu emprestimo é de: " + conta.Valor);
+        Console.WriteLine("Seu emprestimo com Juros é de: " + conta.obterjuros());
+        Console.WriteLine("Suas parcelas mensais serão de: " + conta.Mensal());
+    }
+}
